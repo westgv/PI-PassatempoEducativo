878278aa054d1c7fbb6a59e36494a8827c8364e0
westgv <gustavowestermann@gmail.com>
2023-11-06 12:20:18 -0300

    Commitando alterações no arquivo PI-Passatempo-Educativo

---
+++ script.js
@@ -0,0 +1,9 @@
+const menuToggle = document.getElementById('mobile-menu');
+const menu = document.querySelector('.menu');
+
+menuToggle.addEventListener('click', () => {
+    menu
+        .classList
+        .toggle('active');
+});
+
