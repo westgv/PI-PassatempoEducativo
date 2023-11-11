97265561512e1e06337d67ee1fffba081536de48
VictorNS <victornaokisatosato@gmail.com>
2023-11-11 19:17:40 -0300

    arrumando as pastas

---
+++ Pagina inicial site/script.js
@@ -0,0 +1,12 @@
+const menuToggle = document.getElementById('mobile-menu');
+const menu = document.querySelector('.menu');
+
+menuToggle.addEventListener('click', () => {
+    menu
+        .classList
+        .toggle('active');
+});
+
+//teste
+
+
