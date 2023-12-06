db65f4c9bfe03e3c7eefefd4dc037024b5b5778b
VictorNS <victornaokisatosato@gmail.com>
2023-12-06 09:42:56 -0300

    Inserção de imagens SVG na página Sobre nós

--- paginas/Pagina doacoes/Mobile/JavaScript/script.js
+++
@@ -1,6 +0,0 @@
-const menuToggle = document.getElementById('mobile-menu');
-const menu = document.querySelector('.menu');
-
-menuToggle.addEventListener('click', () => {
-    menu.classList.toggle('active');
-});
